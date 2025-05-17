bda5cd036e21cd56dab7f2c558dae04e92f94ef3
wan064 <paolawan33@gmail.com>
2025-05-17 14:39:33 +0800

    python

---
+++ testQwen.py
@@ -0,0 +1,21 @@
+import os
+from openai import OpenAI
+
+try:
+    client = OpenAI(
+        # If the environment variable is not configured, replace the following line with your API key: api_key="sk-xxx",
+        api_key="sk-09882e1f508c4910b9baa8b4c7a52107",
+        base_url="https://dashscope-intl.aliyuncs.com/compatible-mode/v1",
+    )
+
+    completion = client.chat.completions.create(
+        model="qwen-plus",  # Model list: https://www.alibabacloud.com/help/en/model-studio/getting-started/models
+        messages=[
+            {'role': 'system', 'content': 'You are a helpful assistant.'},
+            {'role': 'user', 'content': 'Who are you?'}
+            ]
+    )
+    print(completion.choices[0].message.content)
+except Exception as e:
+    print(f"Error message: {e}")
+    print("For more information, see: https://www.alibabacloud.com/help/en/model-studio/developer-reference/error-code")
