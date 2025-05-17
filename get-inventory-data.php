3a6d0d34a9cacc0d434df6888b96b7bcd610d7f4
mastura <masturaalya02@gmail.com>
2025-05-17 17:06:38 +0800

    added connection.php

--- get-inventory-data.php
+++ get-inventory-data.php
@@ -1,19 +1,6 @@
 <?php
 // get-inventory-data.php
-
-$conn = new mysqli(
-    "rm-3nsixe3586321q3dsuo.mysql.rds.aliyuncs.com", // ← RDS Public Endpoint rm-3nsixe3586321q3dsuo.mysql.rds.aliyuncs.com
-    "woozi",                 // ← DB user
-    "w@ozi123",                 // ← DB password
-    "caratretail",                      // ← Database name
-    3306                                // ← Port
-);
-
-if ($conn->connect_error) {
-    echo "Connection failed: " . $conn->connect_error;
-} else {
-    echo "Connected to RDS successfully!";
-}
+require_once 'connection.php';
 
 $result = $conn->query("
     SELECT p.product_id, p.product_name, i.quantity_in_stock
